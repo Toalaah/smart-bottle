24b7da86cdf9f18e4f230d17a7e5004a68f8140b
Samuel Kunst <samuel@kunst.me>
2025-05-04 18:23:36 +0200

    feat: re-add basic mock api server

---
+++ backend/models.py
@@ -0,0 +1,27 @@
+import datetime
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class Reading(BaseModel):
+    timestamp: datetime.datetime
+    value: int
+
+
+class ReadingResponse(BaseModel):
+    username: str
+    data: list[Reading]
+
+
+class User(BaseModel):
+    username: str
+    email: str | None = None
+    full_name: str | None = None
+
+
+class DBUser(User):
+    hashed_password: str
