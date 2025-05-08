8d0bb8bc6350b54307e19b1e617e316f2b5cd02f
Samuel Kunst <samuel@kunst.me>
2025-05-08 18:09:38 +0200

    refactor: add structured message exchanges

--- pkg/transport/message.go
+++ pkg/transport/message.go
@@ -1,16 +1,41 @@
 package transport
 
+import (
+	"fmt"
+)
+
 type MessageType uint8
 
 const (
 	HeartBeat MessageType = 1 << iota
-	Battery
 	WaterLevel
-	ConsumptionRate
 )
 
 type Message struct {
-	Length uint8
 	Type   MessageType
+	Length uint8
 	Value  []byte
+}
+
+func (m *Message) Load(b []byte) {
+	m.Value = b
+	m.Length = uint8(len(b))
+}
+
+func (m *Message) MarshalBytes() []byte {
+	return append([]byte{byte(m.Type), m.Length}, m.Value...)
+}
+
+func UnmarshalBytes(m *Message, b []byte) error {
+	if len(b) < 2 {
+		return fmt.Errorf("expected slice of at least length 2")
+	}
+	m.Type = MessageType(b[0])
+	m.Length = b[1]
+	rest := b[2:]
+	if len(rest) < int(m.Length) {
+		return fmt.Errorf("expected payload value to have length of at least %d", m.Length)
+	}
+	m.Value = rest[:m.Length]
+	return nil
 }
---
+++ pkg/transport/message_test.go
@@ -0,0 +1,41 @@
+package transport
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestMessageMarshaling(t *testing.T) {
+	raw := []byte("hello world")
+	msg := &Message{Type: WaterLevel}
+	expected := append([]byte{byte(WaterLevel), byte(len(raw))}, raw...)
+	msg.LoadPayload(raw)
+	got := msg.MarshalBytes()
+	if bytes.Compare(got, expected) != 0 {
+		t.Errorf("Expected decrypted plaintext to be '%v', got '%v'", expected, got)
+	}
+}
+
+func TestMessageUnmarshaling(t *testing.T) {
+	raw := []byte("hello world")
+	msg := &Message{Type: WaterLevel}
+	msg.LoadPayload(raw)
+	payload := msg.MarshalBytes()
+
+	out := &Message{}
+	if err := UnmarshalBytes(out, payload); err != nil {
+		t.Fatal(err)
+	}
+
+	if bytes.Compare(out.Value, raw) != 0 {
+		t.Errorf("Expected decrypted plaintext to be '%v', got '%v'", raw, out.Value)
+	}
+}
+
+func TestMessageUnmarshalingInvalid(t *testing.T) {
+	payload := []byte{byte(WaterLevel), byte(10)}
+	out := &Message{}
+	if err := UnmarshalBytes(out, payload); err == nil {
+		t.Fatal(err)
+	}
+}
